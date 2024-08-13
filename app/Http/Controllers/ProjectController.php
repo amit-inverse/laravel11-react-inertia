ffc65848d4e3ffdc941f4229b7533d5321c17c2d
Amit Kumar Das <amit.inverse@gmail.com>
2024-08-13 23:07:52 +0530

    011 Filtering

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -15,11 +15,19 @@ class ProjectController extends Controller
     public function index()
     {
         $query = Project::query();
-        
+
+        if (request('name')) {
+            $query->where('name', 'like', '%' . request('name') . '%');
+        }
+        if (request('status')) {
+            $query->where('status', request('status'));
+        }
+
         $projects = $query->paginate(10)->onEachSide(1);
 
         return inertia('Project/Index', [
             'projects' => ProjectResouce::collection($projects),
+            'queryParams' => request()->query() ?: null,
         ]);
     }
 
--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,9 +1,28 @@
 import Pagination from "@/Components/Pagination";
+import SelectInput from "@/Components/SelectInput";
+import TextInput from "@/Components/TextInput";
 import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constants";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head, Link } from "@inertiajs/react";
+import { Head, Link, router } from "@inertiajs/react";
 
-export default function index({ auth, projects }) {
+export default function index({ auth, projects, queryParams = null }) {
+  queryParams = queryParams || {};
+  const searchFieldChanged = (name, value) => {
+    if (value) {
+      queryParams[name] = value;
+    } else {
+      delete queryParams[name];
+    }
+
+    router.get(route('project.index'), queryParams);
+  };
+
+  const onKeyPress = (name, e) => {
+    if (e.key !== "Enter") return;
+
+    searchFieldChanged(name, e.target.value);
+  };
+
   return (
     <AuthenticatedLayout
       user={auth.user}
@@ -32,9 +51,47 @@ export default function index({ auth, projects }) {
                     <th className="px-3 py-2 text-right">Actions</th>
                   </tr>
                 </thead>
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
+                  <tr className="text-nowrap">
+                    <th className="px-3 py-2"></th>
+                    <th className="px-3 py-2"></th>
+                    <th className="px-3 py-2">
+                      <TextInput
+                        className="w-full"
+                        defaultValue={queryParams.name}
+                        placeholder="Project Name"
+                        onBlur={(e) =>
+                          searchFieldChanged("name", e.target.value)
+                        }
+                        onKeyPress={(e) => onKeyPress("name", e)}
+                      />
+                    </th>
+                    <th className="px-3 py-2">
+                      <SelectInput
+                        className="w-full"
+                        defaultValue={queryParams.status}
+                        onChange={(e) =>
+                          searchFieldChanged("status", e.target.value)
+                        }
+                      >
+                        <option value="">Select...</option>
+                        <option value="pending">Pending</option>
+                        <option value="in_progress">In Progress</option>
+                        <option value="completed">Completed</option>
+                        </SelectInput>
+                    </th>
+                    <th className="px-3 py-2"></th>
+                    <th className="px-3 py-2"></th>
+                    <th className="px-3 py-2"></th>
+                    <th className="px-3 py-2"></th>
+                  </tr>
+                </thead>
                 <tbody>
                   {projects.data.map((project) => (
-                    <tr key={project.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <tr
+                      key={project.id}
+                      className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                    >
                       <td className="px-3 py-2">{project.id}</td>
                       <td className="px-3 py-2">
                         <img
@@ -45,11 +102,15 @@ export default function index({ auth, projects }) {
                       </td>
                       <td className="px-3 py-2">{project.name}</td>
                       <td className="px-3 py-2">
-                        <span className={"px-2 py-1 rounded text-white " + (PROJECT_STATUS_CLASS_MAP[project.status])}>
-                        {PROJECT_STATUS_TEXT_MAP[project.status]}
-
+                        <span
+                          className={
+                            "px-2 py-1 rounded text-white " +
+                            PROJECT_STATUS_CLASS_MAP[project.status]
+                          }
+                        >
+                          {PROJECT_STATUS_TEXT_MAP[project.status]}
                         </span>
-                        </td>
+                      </td>
                       <td className="px-3 py-2 text-nowrap">
                         {project.created_at}
                       </td>
