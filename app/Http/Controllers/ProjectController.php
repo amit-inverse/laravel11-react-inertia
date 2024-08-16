62de2d64862b967980aef39256f17974d645e99e
Amit Kumar Das <amit.inverse@gmail.com>
2024-08-16 14:29:37 +0530

    020 Project Delete

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -117,6 +117,11 @@ class ProjectController extends Controller
      */
     public function destroy(Project $project)
     {
-        //
+        $name = $project->name;
+
+        $project->tasks()->delete(); 
+        $project->delete();
+
+        return to_route('project.index')->with('success', "Project \"$name\" was deleted");
     }
 }
